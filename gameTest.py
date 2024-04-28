81e87c93edaf24f843e304bf83b861011fb0a7e2
Adrian Janiszewski <127450371+adrianjanisz@users.noreply.github.com>
2024-04-28 14:16:37 -0400

    Delete gameTest.py

--- gameTest.py
+++
@@ -1,8 +0,0 @@
-import Physics;
-
-db = Physics.Database( reset=True )
-db.createDB()
-
-gameInstance = Physics.Game( gameName="Game 01", player1Name="Stefan", player2Name="Efren Reyes" )
-gameInstance.initializeGame(db)
-
